85c2e64ce39c1f89b3daf13c6980a23de3f664e7
Ammar <24FTT1092@student.pb.edu.bn>
2026-04-28 16:03:03 +0800

    ayam

--- app/Http/Controllers/FcmTokenController.php
+++ app/Http/Controllers/FcmTokenController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\FcmToken;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class FcmTokenController extends Controller
 {
@@ -13,6 +14,12 @@ class FcmTokenController extends Controller
         $validated = $request->validate([
             'token' => ['required', 'string', 'max:512'],
             'platform' => ['nullable', 'string', 'in:web,android,ios'],
+        ]);
+
+        Log::info('FCM token store request', [
+            'token_partial' => substr((string) $validated['token'], 0, 32),
+            'user_id' => $request->user()?->id,
+            'platform' => $validated['platform'] ?? 'web',
         ]);
 
         $token = FcmToken::query()->updateOrCreate(
@@ -34,6 +41,10 @@ class FcmTokenController extends Controller
     {
         $validated = $request->validate([
             'token' => ['required', 'string', 'max:512'],
+        ]);
+
+        Log::info('FCM token destroy request', [
+            'token_partial' => substr((string) $validated['token'], 0, 32),
         ]);
 
         FcmToken::query()->where('token', $validated['token'])->delete();
